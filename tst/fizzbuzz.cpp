4aa74f39107e40983a930b5e6a27034d9da3f46b
Wojciech Małek <wojciech_malek@yahoo.co.uk>
2017-10-26 13:50:23 +0200

    fizzbuzz: is "fizz" if contains digit "3"

--- tst/fizzbuzz.cpp
+++ tst/fizzbuzz.cpp
@@ -10,17 +10,31 @@ TEST_CASE("fizzbuzz") {
             33, 36, 39);
 
       for (auto n: nums) {
+         CAPTURE(n);
          REQUIRE(n%3 == 0);
          REQUIRE(n%5 != 0);
          CHECK(fizzbuzz(n) == "fizz");
       }
    }
 
-   SUBCASE("number is \"buzz\" when divisible by 5") {
-      const auto nums = std::experimental::make_array(5, 10, 20, 25, 35, 40,
-            50, 55, 65, 70);
+   SUBCASE("number is \"fizz\" if it contains \"3\" digit") {
+      const auto nums = std::experimental::make_array(13, 23, 31, 32, 34, 37,
+            38, 43, 73, 83);
 
       for (auto n: nums) {
+         CAPTURE(n);
+         REQUIRE(n%3 != 0);
+         REQUIRE(n%5 != 0);
+         CHECK(fizzbuzz(n) == "fizz");
+      }
+   }
+
+   SUBCASE("number is \"buzz\" when divisible by 5") {
+      const auto nums = std::experimental::make_array(5, 10, 20, 25, 40, 50,
+            55, 65, 70, 80, 85, 95);
+
+      for (auto n: nums) {
+         CAPTURE(n);
          REQUIRE(n%5 == 0);
          REQUIRE(n%3 != 0);
          CHECK(fizzbuzz(n) == "buzz");
@@ -28,9 +42,11 @@ TEST_CASE("fizzbuzz") {
    }
 
    SUBCASE("number is \"fizzbuzz\" when divisible both by 3 and 5") {
-      const auto nums = std::experimental::make_array(15, 30, 45, 60, 75, 90);
+      const auto nums = std::experimental::make_array(15, 30, 45, 60, 75,
+            90);
 
       for (auto n: nums) {
+         CAPTURE(n);
          REQUIRE(n%3 == 0);
          REQUIRE(n%5 == 0);
          CHECK(fizzbuzz(n) == "fizzbuzz");
@@ -38,10 +54,11 @@ TEST_CASE("fizzbuzz") {
    }
 
    SUBCASE("translate number to text when not divisible by 3 or 5") {
-      const auto nums = std::experimental::make_array(1, 2, 4, 7, 8, 11, 13,
+      const auto nums = std::experimental::make_array(1, 2, 4, 7, 8, 11,
             14, 16, 17, 19);
 
       for (auto n: nums) {
+         CAPTURE(n);
          REQUIRE(n%3 != 0);
          REQUIRE(n%5 != 0);
          CHECK(fizzbuzz(n) == std::to_string(n));
