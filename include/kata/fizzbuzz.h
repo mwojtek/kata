b9c6cacd068aa1d04e500a555f715f7bc16250ca
Wojciech Małek <wojciech_malek@yahoo.co.uk>
2017-10-26 13:42:55 +0200

    fizzbuzz: minor refactoring

--- include/kata/fizzbuzz.h
+++ include/kata/fizzbuzz.h
@@ -4,11 +4,23 @@
 
 namespace kata {
 
+namespace detail {
+
+constexpr bool is_fizz(int n) noexcept {
+   return n%3 == 0;
+}
+
+constexpr bool is_buzz(int n) noexcept {
+   return n%5 == 0;
+}
+
+}
+
 inline std::string fizzbuzz(int n) {
    std::ostringstream oss;
-   if (n%3 == 0)
+   if (detail::is_fizz(n))
       oss << "fizz";
-   if (n%5 == 0)
+   if (detail::is_buzz(n))
       oss << "buzz";
    return oss.str().empty() ? std::to_string(n) : oss.str();
 }
