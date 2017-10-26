d85409067c1256ebbd93f7876bf1d7aeb46c5484
Wojciech Małek <wojciech_malek@yahoo.co.uk>
2017-10-26 13:51:21 +0200

    fizzbuzz: is "fizz" when contains digit "3" implementation

--- include/kata/fizzbuzz.h
+++ include/kata/fizzbuzz.h
@@ -6,8 +6,17 @@ namespace kata {
 
 namespace detail {
 
+constexpr bool has_digit(int n, int m) noexcept {
+   while (n > 0) {
+      if (n%10 == m)
+         return true;
+      n /= 10;
+   }
+   return false;
+}
+
 constexpr bool is_fizz(int n) noexcept {
-   return n%3 == 0;
+   return n%3 == 0 || has_digit(n, 3);
 }
 
 constexpr bool is_buzz(int n) noexcept {
