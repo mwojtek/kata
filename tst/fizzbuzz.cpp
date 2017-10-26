9fd5e7150d764221f0bf1379f6801033b7181cd9
Wojciech Małek <wojciech_malek@yahoo.co.uk>
2017-10-26 11:51:18 +0200

    fizzbuzz: add test for "fizzbuzz"

--- tst/fizzbuzz.cpp
+++ tst/fizzbuzz.cpp
@@ -5,7 +5,7 @@
 using namespace kata;
 
 TEST_CASE("fizzbuzz") {
-   SUBCASE("number is fizz when divisible by 3") {
+   SUBCASE("number is \"fizz\" when divisible by 3") {
       const auto nums = std::experimental::make_array(3, 6, 9, 12, 18, 21, 24, 27,
             33, 36, 39);
 
@@ -16,7 +16,7 @@ TEST_CASE("fizzbuzz") {
       }
    }
 
-   SUBCASE("number is buzz when divisible by 5") {
+   SUBCASE("number is \"buzz\" when divisible by 5") {
       const auto nums = std::experimental::make_array(5, 10, 20, 25, 35, 40,
             50, 55, 65, 70);
 
@@ -24,6 +24,16 @@ TEST_CASE("fizzbuzz") {
          REQUIRE(n%5 == 0);
          REQUIRE(n%3 != 0);
          CHECK(fizzbuzz(n) == "buzz");
+      }
+   }
+
+   SUBCASE("number is \"fizzbuzz\" when divisible both by 3 and 5") {
+      const auto nums = std::experimental::make_array(15, 30, 45, 60, 75, 90);
+
+      for (auto n: nums) {
+         REQUIRE(n%3 == 0);
+         REQUIRE(n%5 == 0);
+         CHECK(fizzbuzz(n) == "fizzbuzz");
       }
    }
 }
