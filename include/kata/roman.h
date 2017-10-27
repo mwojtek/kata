86a8db0df54e0da846b78ad1fe130752e11c0734
Wojciech Małek <wojciech_malek@yahoo.co.uk>
2017-10-27 10:25:51 +0200

    roman numerals: parse number "4"

--- include/kata/roman.h
+++ include/kata/roman.h
@@ -5,6 +5,10 @@ namespace kata {
 
 inline std::string to_roman(int n) {
    std::stringstream oss;
+   if (n > 3) {
+      n -= 4;
+      oss << "IV";
+   }
    while (n-- > 0)
       oss << "I";
    return oss.str();
