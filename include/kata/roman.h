4e406b079ea158e0abb59e043211b881ec38b1c3
Wojciech Małek <wojciech_malek@yahoo.co.uk>
2017-10-27 10:41:18 +0200

    roman numerals: parse number "9"

--- include/kata/roman.h
+++ include/kata/roman.h
@@ -5,8 +5,8 @@
 namespace kata {
 
 inline std::string to_roman(int n) {
-   constexpr auto nums = std::experimental::make_array(std::make_pair(5, "V"),
-         std::make_pair(4, "IV"), std::make_pair(1, "I"));
+   constexpr auto nums = std::experimental::make_array(std::make_pair(9, "IX"),
+         std::make_pair(5, "V"), std::make_pair(4, "IV"), std::make_pair(1, "I"));
    std::stringstream oss;
    for (const auto& d: nums) {
       while (n >= d.first) {
