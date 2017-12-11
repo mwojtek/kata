8947a4ccc518c7f50ff9d152e94e838f9839db6f
Wojciech Małek <wojciech_malek@yahoo.co.uk>
2017-12-11 12:26:10 +0100

    word wrap: I think it's done

--- include/kata/word_wrap.h
+++ include/kata/word_wrap.h
@@ -1,10 +1,27 @@
 #pragma once
-#include <sstream>
+#include <string>
+
 
 namespace kata {
 
-inline std::string word_wrap(const std::string& str, int n) noexcept {
-   return {};
+
+inline void word_wrap_in_place(std::string& txt, unsigned n) {
+   for (auto i = n; i < txt.size(); ) {
+      auto s = txt.rfind(' ', i);
+      if (s ==  std::string::npos)
+         s = txt.find(' ', i);
+      if (s == std::string::npos)
+         break;
+      txt.at(s) = '\n';
+      i += s;
+   }
 }
+
+
+inline std::string word_wrap(std::string txt, unsigned n) {
+   word_wrap_in_place(txt, n);
+   return txt;
+}
+
 
 }
--- tst/word_wrap.cpp
+++ tst/word_wrap.cpp
@@ -4,5 +4,12 @@
 using namespace kata;
 
 TEST_CASE("word wrap") {
-   CHECK(false);
+   CHECK(word_wrap("", 0) == "");
+   CHECK(word_wrap("some text", 9) == "some text");
+   CHECK(word_wrap("some text", 4) == "some\ntext");
+   CHECK(word_wrap("some text", 5) == "some\ntext");
+   CHECK(word_wrap("some text", 3) == "some\ntext");
+   CHECK(word_wrap("some other text", 8) == "some\nother\ntext");
+   CHECK(word_wrap("verylongtextwithnospace", 1) == "verylongtextwithnospace");
+   CHECK(word_wrap("wrap every space available", 0) == "wrap\nevery\nspace\navailable");
 }
